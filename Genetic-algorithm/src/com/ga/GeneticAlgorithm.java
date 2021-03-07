d33963e89da9bcada403bceaa8a204b07890b9dc
Gerasimov Alexey <gerasimov2122@icloud.com>
2021-03-07 18:42:21 +0300

    +roulette

--- Genetic-algorithm/src/com/ga/GeneticAlgorithm.java
+++ Genetic-algorithm/src/com/ga/GeneticAlgorithm.java
@@ -1,11 +1,34 @@
 package com.ga;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class GeneticAlgorithm {
+    int numOfGenomes;
+    int genomeLength;
 
-  int genomeLength;
+    public GeneticAlgorithm() {
 
-  public GeneticAlgorithm() {
+    }
 
-  }
+    public Genome roulette(List<Genome> genomes) {
+        Random random = new Random();
+        List<Double> wheel = new ArrayList<>();
+        wheel.add(1 / genomes.get(0).getFitness());
+        for (int i = 1; i < genomes.size(); i++) {
+            wheel.add(1 / genomes.get(i).getFitness() + wheel.get(i - 1));
+        }
+        double randomNumber = random.nextDouble() * wheel.get(wheel.size() - 1);
+
+        for (int i = 0; i < genomes.size(); i++) {
+            if (wheel.get(i) >= randomNumber) {
+                Genome result = genomes.get(i);
+                genomes.remove(i);
+                return result;
+            }
+        }
+        return null;
+    }
 
 }
--- Genetic-algorithm/src/com/ga/Main.java
+++ Genetic-algorithm/src/com/ga/Main.java
@@ -13,12 +13,28 @@ public class Main {
 
         List<City> cities = new ArrayList<>();
         City city1 = new City(1, 2, 2);
-        City city2 = new City(2,2,4);
+        City city2 = new City(2, 2, 4);
+        City city3 = new City(3, 2, 6);
+        City city4 = new City(4, 2, 8);
         cities.add(city1);
         cities.add(city2);
+        cities.add(city3);
+        cities.add(city4);
         cities.add(city1);
         Genome genome = new Genome(cities);
         System.out.println(genome.getFitness());
-
+        List<City> cities2 = new ArrayList<>();
+        cities2.add(city1);
+        cities2.add(city4);
+        cities2.add(city2);
+        cities2.add(city3);
+        cities2.add(city1);
+        Genome genome2 = new Genome(cities2);
+        System.out.println(genome2.getFitness());
+        List<Genome> genomes = new ArrayList<>();
+        genomes.add(genome);
+        genomes.add(genome2);
+        GeneticAlgorithm ga = new GeneticAlgorithm();
+        System.out.println("Roulette said: " + ga.roulette(genomes).getFitness());
     }
 }
