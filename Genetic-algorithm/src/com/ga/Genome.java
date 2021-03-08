2fbfc848434cdbe18197622c530132e32428796c
VadMack <makarovvadim01@icloud.com>
2021-03-08 14:39:07 +0300

    sequence from list to array

--- Genetic-algorithm/src/com/ga/Genome.java
+++ Genetic-algorithm/src/com/ga/Genome.java
@@ -3,42 +3,40 @@ package com.ga;
 import static com.ga.SupportClass.isPrime;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 public class Genome {
   private int length;
   private double fitness;
   private List<City> cities;
-  private List<Integer> sequence;
+  Integer[] sequence;
 
   public Genome(List<City> cities) {
     length = cities.size();
     this.cities = cities;
-    sequence = cities.stream().map(City::getId).collect(Collectors.toList());
-    //fitness = calculateFitness();
+    sequence = cities.stream().map(City::getId).toArray(Integer[]::new);
+    fitness = calculateFitness();
   }
 
   public Genome(Genome genome){
     this.length = genome.getLength();
     this.fitness = genome.getFitness();
     this.cities = new ArrayList<>(genome.getCities());
-    this.sequence = new ArrayList<>(genome.getSequence());
+    this.sequence = Arrays.copyOf(genome.getSequence(), genome.getLength());
   }
 
   double calculateFitness() {
-    //long time = System.currentTimeMillis();
     double distance = 0;
     for (int i = 0; i < length - 1; i++) {
-      double toAdd = Math.sqrt(Math.pow(cities.get(sequence.get(i)).getX() - cities.get(sequence.get(i + 1)).getX(), 2) +
-          Math.pow(cities.get(sequence.get(i)).getY() - cities.get(sequence.get(i + 1)).getY(), 2));
-      if (!isPrime(sequence.get(i)) && (i % 10 == 0) && (i != 0)) {
+      double toAdd = Math.sqrt(Math.pow(cities.get(sequence[i]).getX() - cities.get(sequence[i + 1]).getX(), 2) +
+          Math.pow(cities.get(sequence[i]).getY() - cities.get(sequence[i + 1]).getY(), 2));
+      if (!isPrime(sequence[i]) && (i % 10 == 0) && (i != 0)) {
         toAdd *= 1.1;
       }
       distance += toAdd;
     }
-    //System.out.println((double) System.currentTimeMillis() - time);
     return  distance;
   }
 
@@ -62,11 +60,11 @@ public class Genome {
     this.cities = cities;
   }
 
-  public List<Integer> getSequence() {
+  public Integer[] getSequence() {
     return sequence;
   }
 
-  public void setSequence(List<Integer> sequence) {
+  public void setSequence(Integer[] sequence) {
     this.sequence = sequence;
   }
 
@@ -90,7 +88,7 @@ public class Genome {
         "length=" + length +
         ", fitness=" + fitness +
         ", cities=" + cities +
-        ", sequence=" + sequence +
+        ", sequence=" + Arrays.toString(sequence) +
         '}';
   }
 }
