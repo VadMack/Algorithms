0015b8f55b345eb24bd9b83e2e3cccfcda43c523
VadMack <makarovvadim01@icloud.com>
2021-03-08 10:50:21 +0300

    optimisation

--- Genetic-algorithm/src/com/ga/Genome.java
+++ Genetic-algorithm/src/com/ga/Genome.java
@@ -4,6 +4,7 @@ import static com.ga.SupportClass.isPrime;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Genome {
@@ -26,32 +27,19 @@ public class Genome {
     this.sequence = new ArrayList<>(genome.getSequence());
   }
 
-  // TODO
   double calculateFitness() {
+    long time = System.currentTimeMillis();
     double distance = 0;
     for (int i = 0; i < length - 1; i++) {
-      double toAdd = Math.sqrt(Math.pow(findCityById(sequence.get(i)).getX() - findCityById(sequence.get(i + 1)).getX(), 2) +
-          Math.pow(findCityById(sequence.get(i)).getY() - findCityById(sequence.get(i + 1)).getY(), 2));
+      double toAdd = Math.sqrt(Math.pow(cities.get(sequence.get(i)).getX() - cities.get(sequence.get(i + 1)).getX(), 2) +
+          Math.pow(cities.get(sequence.get(i)).getY() - cities.get(sequence.get(i + 1)).getY(), 2));
       if (!isPrime(sequence.get(i)) && (i % 10 == 0) && (i != 0)) {
         toAdd *= 1.1;
       }
       distance += toAdd;
     }
+    System.out.println((double) System.currentTimeMillis() - time);
     return  distance;
-  }
-
-  @Override
-  public boolean equals(Object object){
-    return object instanceof Genome && this.sequence.equals(((Genome) object).sequence);
-  }
-
-  private City findCityById(int id){
-    for (City city : cities) {
-      if (city.getId() == id) {
-        return city;
-      }
-    }
-    return null;
   }
 
   public int getLength() {
@@ -84,6 +72,26 @@ public class Genome {
 
   public double getFitness() {
     return fitness;
+  }
+
+  @Override
+  public boolean equals(Object object){
+    return object instanceof Genome && this.sequence.equals(((Genome) object).sequence);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(length, fitness, cities, sequence);
+  }
+
+  @Override
+  public String toString() {
+    return "Genome{" +
+        "length=" + length +
+        ", fitness=" + fitness +
+        ", cities=" + cities +
+        ", sequence=" + sequence +
+        '}';
   }
 }
 
